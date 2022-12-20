c761cdd97cc5e353bafd043b60e99684d61b1df1
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-12-20 16:16:17 +0300

    update setup

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
   devServer: {
     static: './dist',
   },
@@ -17,8 +20,31 @@ module.exports = {
     clean: true,
     publicPath: '/',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
   optimization: {
     runtimeChunk: 'single',
   },
-  
 };
